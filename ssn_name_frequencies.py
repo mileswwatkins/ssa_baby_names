cb32f0346aeb5b269c2912ccc9e2bc8ebc53cfbf
Miles Watkins <mileswwatkins@gmail.com>
2014-09-04 23:25:18 -0400

    Completed basics of parse_table()

--- ssn_name_frequencies.py
+++ ssn_name_frequencies.py
@@ -1,4 +1,5 @@
 from datetime.date import today
+import re
 
 from xml.etree import cElementTree as ET
 from requests import post
@@ -20,7 +21,7 @@ def check_parameters(year, name_gender_is_male, count_returned):
 
     # Check that name_gender_is_male is a boolean
     if type(name_gender_is_male) is not bool:
-        raise TypeError("name_gender_is_male must be True (returns male names) or False (female names)")
+        raise TypeError("name_gender_is_male must be True (returns male names) or False (female)")
 
     # Check that count_returned is an integer between 1 and 1000
     LOWEST_COUNT_ALLOWED = 1
@@ -28,7 +29,7 @@ def check_parameters(year, name_gender_is_male, count_returned):
     if type(count_returned) is not int or \
             count_returned < LOWEST_COUNT_ALLOWED or \
             count_returned > HIGHEST_COUNT_ALLOWED:
-        raise ValueError("count_returned must be a positive integer between {0} and {1}".format(
+        raise ValueError("count_returned must be an integer between {0} and {1}".format(
                 (LOWEST_COUNT_ALLOWED, HIGHEST_COUNT_ALLOWED)))
 
 def get_response_from_ssa(year, percentage_instead_of_frequency):
@@ -64,25 +65,55 @@ def parse_table(returned_html):
     '''Extract the table data from the provided HTML'''
     
     # Extract the names table from the full HTML page
-    
+    # There are multiple <table>s,
+    # use the one with <... summary="Popularity for top [number]...">
+    name_table_search = r'''.+(<table.*?summary="Popularity for top \d{1,4}.+?<\/table>).+'''
+    table_html = re.findall(name_table_search, returned_html, re.DOTALL)[0]
 
     # Parse the table into memory
     table_parser = ET.XML(table_html)
-    table = []
-
     rows = iter(table_parser)
+
+    # Confirm that the column names are as expected
+    EXPECTED_COLUMN_NAMES = [
+            "Rank",
+            "Male name",
+            "Number of males",
+            "Female name",
+            "Number of females"
+            ]
     column_names = [header.text for header in next(rows)]
+    if column_names is not EXPECTED_COLUMN_NAMES:
+        raise Error("SSA's Web API has changed, please check for a package update")
+
+    # Finish parsing the table rows
+    male_table = []
+    female_table = []
     for row in rows:
         values = [value.text for value in row]
-        table.append(dict(zip(column_names, values)))
+        values_labeled = dict(zip(COLUMN_NAMES_TO_USE, values))
 
+        # Restructure the columns so that male and female data are separated
+        male_data = {
+                "name": values_labeled["Male name"],
+                "value": values_labled["Number of males"]
+                }
+        male_table.append(male_data)
+
+        female_data = {
+                "name": values_labeled["Female name"],
+                "value": values_labled["Number of females"]
+                }
+        female_table.append(female_data)
+
+    table = {"male": male_data, "female": female_data}
     return table
 
 def main(year, name_gender_is_male, count_returned=1000):
     '''
     The main function of this package, which returns a list of the top
     names of a given gender for a given year, and their frequencies
-    and relative frequencies (ie, percentages)
+    and relative frequencies (ie, percentages of overall population)
     '''
 
     # Check user-provided parameters, throwing an error if they are invalid
