650f2f80c1e457523951137442ecf04479613e05
Miles Watkins <mileswwatkins@gmail.com>
2014-09-28 15:09:10 -0400

    Added __init__.py to fix pip installation

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
         name="ssa_baby_names",
         packages=["ssa_baby_names"],
-        version="1.1",
+        version="1.2",
         description="Python wrapper for the Social Security Administration's Popular Baby Names service",
         author="Miles Watkins",
         author_email="miles.w.watkins@gmail.com",
         url="https://github.com/mileswwatkins/ssa_baby_names",
-        download_url="https://github.com/mileswwatkins/ssa_baby_names/tarball/1.1",
+        download_url="https://github.com/mileswwatkins/ssa_baby_names/tarball/1.2",
         keywords=["Social Security Administaration", "SSA", "baby names", "popular names", "names"],
         classifiers=[],
         )
